f54d6414ba1051d114df3feba0983fd3db2794b6
Fabian Schmitthenner <development@schmitthenner.eu>
2023-08-28 21:41:39 +0200

    n

--- xetex.nix
+++ xetex.nix
@@ -7,6 +7,13 @@ buildEmscriptenPackage rec {
 
   nativeBuildInputs = [ pkg-config ];
 
+  configurePhase = ''
+  '';
+
+  buildPhase = ''
+    ./build.sh
+  '';
+
   src = fetchurl {
     url = "http://downloads.sourceforge.net/project/xetex/source/xetex-0.9999.3.tar.bz2";
     sha256 = "sha256-B0GkBX+Anh9GYXq93zXiVX9NIF5Lfzw4h/MWzubhVOU";
